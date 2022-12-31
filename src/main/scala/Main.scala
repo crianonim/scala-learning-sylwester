cdf7c4f7a40568bc3893c5ee47f1ebde3bc7aedc
Jan Skowronski <jan@permutive.com>
2022-12-31 12:59:08 +0000

    simple route

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,25 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello world!")
+import cats.Monad
+import cats.effect.{ExitCode, IO, IOApp}
+import org.http4s.HttpRoutes
+import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.dsl._
+import org.http4s.implicits._
+
+object Main extends IOApp {
+  def run(args: List[String]): IO[ExitCode] = {
+    def helloRoute[F[_] : Monad]: HttpRoutes[F] = {
+      val dsl = Http4sDsl[F]
+      import dsl._
+      HttpRoutes.of[F] {
+        case GET -> Root => (Ok("Hello!"))
+      }
+    }
+
+    BlazeServerBuilder[IO](runtime.compute)
+      .bindHttp(8080, "localhost")
+      .withHttpApp(helloRoute[IO].orNotFound)
+      .resource
+      .use(_ => IO.never)
+      .as(ExitCode.Success)
   }
 }
